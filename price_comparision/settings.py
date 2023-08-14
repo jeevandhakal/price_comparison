a0ee6bc5c93105a9979cfde57c062d7912841441
je-evan <dhakaljeevan165@gmail.com>
2023-08-14 20:55:55 +0545

    feat: vercel deployment settings

--- price_comparision/settings.py
+++ price_comparision/settings.py
@@ -18,7 +18,7 @@ SECRET_KEY = os.environ['DJANGO_KEY']
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', '.vercel.app']
 
 
 # Application definition
