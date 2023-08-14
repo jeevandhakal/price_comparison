526bf7a6560fd148f4a4eff1de71fc11e77124fa
je-evan <dhakaljeevan165@gmail.com>
2023-08-14 22:15:33 +0545

    refactor: unwanted packages are removed from requirements.txt

--- price_comparision/settings.py
+++ price_comparision/settings.py
@@ -33,7 +33,9 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'user.apps.UserConfig',
+
     'crispy_forms',
+    'crispy_bootstrap4',
 ]
 
 MIDDLEWARE = [
